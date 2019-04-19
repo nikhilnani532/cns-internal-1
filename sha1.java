85488dfcb6393b1db3d69d9bf1802aefe1c15f64
prathul sai <32157444+prathulsai@users.noreply.github.com>
2019-04-19 19:32:39 +0530

    Update sha1.java

--- sha1.java
+++ sha1.java
@@ -1,7 +1,7 @@
 import java.math.BigInteger;
 import java.security.MessageDigest;
 class sha1{
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception{
 		try{
 		String input="hello world";
 		MessageDigest md=MessageDigest.getInstance("SHA-1");
