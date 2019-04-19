75894342dd5c1638fedeb887e12dd800ee64189d
prathul sai <32157444+prathulsai@users.noreply.github.com>
2019-04-19 20:07:31 +0530

    Add files via upload

---
+++ blowfish.java
@@ -0,0 +1,18 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.swing.JOptionPane;
+class blowfish{
+	public static void main(String[] args) throws Exception{
+		KeyGenerator kg=KeyGenerator.getInstance("blowfish");
+		SecretKey sk=kg.generateKey();
+		Cipher ch=Cipher.getInstance("blowfish");
+		ch.init(Cipher.ENCRYPT_MODE,sk);
+		String input=JOptionPane.showInputDialog("enter input");
+		byte[] emsg=ch.doFinal(input.getBytes());
+		ch.init(Cipher.DECRYPT_MODE,sk);
+		byte[] dmsg=ch.doFinal(emsg);
+		JOptionPane.showMessageDialog(JOptionPane.getRootFrame(),"encrypted : " + new String(emsg)+"\ndecrypted : " + new String(dmsg));
+		System.exit(0);
+	}
+}
